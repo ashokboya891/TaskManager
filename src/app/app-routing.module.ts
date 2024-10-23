346871e053c564e2fa15f345eb75fe53c6226503
ashokboya891 <ashokboya891@gmail.com>
2024-10-23 11:42:54 +0530

    Module added and update with components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { AboutComponent } from './about/about.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { AboutComponent } from './admin/about/about.component';
+import { MyProfileComponent } from './admin/my-profile/my-profile.component';
 
 const routes: Routes = [
   {
@@ -9,6 +10,9 @@ const routes: Routes = [
   },
   {
     path:"about",component:AboutComponent
+  },
+  {
+    path:"myProfile",component:MyProfileComponent
   },
   {
    path:"", redirectTo:"dashboard",pathMatch:"full"
